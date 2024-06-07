18d0cf7b16556db7d85a874e589a6fdfbb4f70e5
PranjalG1122 <pranjalg1122@gmail.com>
2024-06-07 15:34:28 +0530

    style: styled signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,44 @@
+import { Button, variants } from "@/components/Button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function SignUp() {
-  return <main className=""></main>;
+  return (
+    <main className="flex flex-col items-center justify-center w-full flex-1 min-h-full bg-hero bg-no-repeat bg-cover bg-center bg-fixed px-4">
+      <div className="flex flex-col items-center gap-6 bg-gray-50 rounded w-full max-w-md lg:py-32 py-16 lg:px-16 px-8">
+        <Image
+          src="/logo.png"
+          width={240}
+          height={40}
+          alt="logo"
+          unoptimized={true}
+          className=""
+        />
+        <h1 className="font-bold lg:text-3xl text-2xl">Sign In</h1>
+        <form className="flex flex-col items-center gap-6 w-full">
+          <div className="flex flex-col items-center gap-2 w-full">
+            <input
+              className={variants({ variant: "input" })}
+              placeholder="Name"
+            />
+            <input
+              className={variants({ variant: "input" })}
+              placeholder="Email"
+            />
+            <input
+              className={variants({ variant: "input" })}
+              placeholder="Password"
+            />
+          </div>
+
+          <Button type="submit" className="w-full">
+            Submit
+          </Button>
+          <p>Already have an account? Sign in <Link href="/signin"
+            className="text-indigo-600 hover:underline focus:outline-none transition-all"
+          >here</Link>.</p>
+        </form>
+      </div>
+    </main>
+  );
 }
---
+++ components/Button.tsx
@@ -0,0 +1,38 @@
+import { cva, VariantProps } from "class-variance-authority";
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+export const variants = cva("rounded-sm font-regular lg:text-base text-sm", {
+  variants: {
+    variant: {
+      primary:
+        "px-4 py-2 bg-indigo-600 rounded font-medium text-white lg:text-base text-sm hover:bg-indigo-700 focus:outline-none transition-all",
+      secondary:
+        "",
+      link: "",
+      input: "bg-gray-50 border border-gray-300 w-full px-4 py-2 focus:outline-none focus:ring-1 focus:ring-gray-200 rounded font-regular lg:text-base text-sm",
+      icon: "lg:h-6 lg:w-6 h-5 w-5",
+      iconButton:
+        "",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+  },
+});
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof variants> {}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={twMerge(variants({ variant, className }))}
+        {...props}
+      />
+    );
+  }
+);
