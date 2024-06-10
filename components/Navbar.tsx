0fa2c7b9b003d277a743f2ce7d99a674f16f6afb
LakshyaGupta26 <lakshyagupta2602@gmail.com>
2024-06-10 18:02:06 +0530

    feat: created navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,70 +1,21 @@
 "use client";
 
-import styled from 'styled-components';
-
-const NavbarContainer = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px 50px;
-  background-color: #fff;
-  border-bottom: 1px solid #ddd;
-`;
-
-const Logo = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 24px;
-  color: #333;
-
-  img {
-    margin-right: 10px;
-  }
-`;
-
-const NavLinks = styled.div`
-  a {
-    margin-left: auto;
-    margin-right: auto;
-    padding: 0 20px;
-    color: #333;
-    text-decoration: none;
-    font-size: 18px;
-    &:hover {
-      color: #666;
-    }
-  }
-`;
-
-const SignUpButton = styled.button`
-  position: relative;
-  float: right;
-  background-color: #6c63ff;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  padding: 10px 20px;
-  font-size: 18px;
-  cursor: pointer;
-  &:hover {
-    background-color: #5753c9;
-  }
-  
-`;
+import React from 'react';
+import Image from 'next/image';
 
 const Navbar: React.FC = () => {
   return (
-    <NavbarContainer>
-      <Logo>
-        <img src="/logo.png" alt="Logo" /> COLEARN
-      </Logo>
-      <NavLinks>
-        <a href="#about">About Us</a>
-        <a href="#courses">Courses</a>
-        <a href="#contact">Contact Us</a>
-      </NavLinks>
-      <SignUpButton>Sign Up</SignUpButton>
-    </NavbarContainer>
+    <nav className="flex justify-between items-center p-5 bg-white border-b border-gray-200">
+      <div className="flex items-center text-2xl text-gray-800">
+        <a href="" className="mr-2"><img src="/logo.png" alt="Logo Colearn" width={250} height={250} /></a>
+      </div>
+      <div>
+        <a href="#about" className="mx-4 text-lg text-gray-800 hover:text-gray-600">About Us</a>
+        <a href="#courses" className="mx-4 text-lg text-gray-800 hover:text-gray-600">Courses</a>
+        <a href="#contact" className="mx-4 text-lg text-gray-800 hover:text-gray-600">Contact Us</a>
+      </div>
+      <button className="bg-indigo-600 text-white rounded px-4 py-2 text-lg hover:bg-purple-500">Sign Up</button>  
+    </nav>
   );
 };
 
