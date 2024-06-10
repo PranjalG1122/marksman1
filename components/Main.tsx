1eea576e2d57393e37c4746cba37c52d890049c0
LakshyaGupta26 <lakshyagupta2602@gmail.com>
2024-06-10 18:50:43 +0530

    feat: created homepage

--- components/Main.tsx
+++ components/Main.tsx
@@ -6,17 +6,17 @@ const Main: React.FC = () => {
   return (
     <main className="flex justify-between items-center p-12 bg-gray-50">
       <div className="max-w-md">
-        <h2 className="text-2xl text-purple-600 mb-4">The best AI adaptive online course</h2>
+        <h2 className="text-2xl text-indigo-600 mb-4">The best AI adaptive online course</h2>
         <h1 className="text-4xl font-bold mb-4">
           Find the best <span className="text-purple-600">courses</span> here
         </h1>
         <p className="text-lg mb-4">Get a personalized learning experience that adapts to your needs. Get started with CoLearn today!</p>
         <div className="flex space-x-4">
-          <button className="bg-purple-600 text-white rounded px-4 py-2 text-lg hover:bg-purple-500">Get Started</button>
-          <button className="bg-white text-purple-600 border border-purple-600 rounded px-4 py-2 text-lg hover:bg-gray-100">Learn More</button>
+          <button className="bg-indigo-600 text-white rounded px-4 py-2 text-lg hover:bg-purple-500">Get Started</button>
+          <button className="bg-white text-indigo-600 border border-purple-600 rounded px-4 py-2 text-lg hover:bg-gray-100">Learn More</button>
         </div>
       </div>
-      <div className="flex flex-wrap gap-4 w-1/2">
+      <div className="flex flex-wrap gap-2 w-1/2">
         <img src="/home1.png" alt="Image 1" className="w-1/2 rounded-lg" />
         <img src="/home2.png" alt="Image 2" className="w-1/2 rounded-lg" />
         <img src="/home3.png" alt="Image 3" className="w-1/2 rounded-lg" />
