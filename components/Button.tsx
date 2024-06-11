d9e8402bc8d9b80f18509b006060a8e91689dd95
LakshyaGupta26 <lakshyagupta2602@gmail.com>
2024-06-11 15:32:01 +0530

    style: secondary

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,7 +7,8 @@ export const variants = cva("rounded-sm font-regular lg:text-base text-sm", {
     variant: {
       primary:
         "px-4 py-2 bg-indigo-600 rounded font-medium text-white lg:text-base text-sm hover:bg-indigo-700 focus:outline-none transition-all",
-      secondary: "",
+      secondary:
+        "bg-white text-indigo-600 border border-purple-600 rounded px-4 py-2 text-lg hover:bg-gray-100",
 
       input:
         "bg-gray-50 border border-gray-300 w-full px-4 py-2 focus:outline-none focus:ring-1 focus:ring-gray-200 rounded font-regular lg:text-base text-sm",
