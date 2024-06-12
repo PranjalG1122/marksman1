87b0d3847d696c99e76258b90737d5b481026ff7
LakshyaGupta26 <lakshyagupta2602@gmail.com>
2024-06-12 23:55:47 +0530

    refactor: update page component to use Contact component instead of ContactForm and ContactDetails

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,15 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Main from "@/components/Main";
-import ContactForm from "@/components/ContactForm";
-import ContactDetails from "@/components/ContactDetails";
+import Contact from "@/components/Contact";
 
 const Home: React.FC = () => {
   return (
     <div className="flex min-h-screen flex-col items-center w-full">
       <Navbar />
-      <Main />
-      <section className="flex flex-col items-center justify-center py-12 bg-gray-50 min-h-container">
-        <div className="flex flex-col md:flex-row w-full p-4 md:p-8 max-w-7xl mx-auto">
-          <div className="md:w-1/2 p-4">
-            <ContactDetails />
-          </div>
-          <div className="md:w-1/2 p-4">
-            <ContactForm />
-          </div>
-        </div>
-      </section>
+      <Main /> 
+      <Contact />
     </div>
   );
 };
-
 export default Home;
