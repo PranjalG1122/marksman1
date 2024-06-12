9cf7fa266a5ba22d0b65b90ede4b4a2529fff4fd
PranjalG1122 <pranjalg1122@gmail.com>
2024-06-12 22:51:22 +0530

    fix: added try catch to verifyToken

--- server/verifyToken.ts
+++ server/verifyToken.ts
@@ -4,15 +4,20 @@ import { cookies } from "next/headers";
 import { verifyJoseToken } from "@/server/jose";
 
 export const verifyToken = async (): Promise<boolean> => {
-  const cookie = cookies();
+  try {
+    const cookie = cookies();
 
-  const token = cookie.get("token");
-  if (!token) return false;
+    const token = cookie.get("token");
+    if (!token) return false;
 
-  const decodedToken = await verifyJoseToken(token.value);
-  if (!decodedToken.payload.id) return false;
-  if (!decodedToken.payload.exp) return false;
-  if (decodedToken.payload.exp < Date.now() / 1000) return false;
+    const decodedToken = await verifyJoseToken(token.value);
+    if (!decodedToken.payload.id) return false;
+    if (!decodedToken.payload.exp) return false;
+    if (decodedToken.payload.exp < Date.now() / 1000) return false;
 
-  return true;
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
 };
