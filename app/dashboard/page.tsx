bb70bf192dcc854720c2c0e366a3b202f7ed987f
PranjalG1122 <pranjalg1122@gmail.com>
2024-06-14 23:03:04 +0530

    refactor: border style + changed to Link

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,6 +9,7 @@ import class4 from "@/public/mascots/class4.png";
 import class5 from "@/public/mascots/class5.png";
 
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Dashboard() {
   const classes = [
@@ -16,26 +17,31 @@ export default function Dashboard() {
       mascot: class1,
       color: "bg-yellow-400",
       border: "border-yellow-500",
+      hover: "hover:bg-yellow-500",
     },
     {
       mascot: class2,
       color: "bg-blue-400",
       border: "border-blue-500",
+      hover: "hover:bg-blue-500",
     },
     {
       mascot: class3,
       color: "bg-purple-400",
       border: "border-purple-500",
+      hover: "hover:bg-purple-500",
     },
     {
       mascot: class4,
       color: "bg-pink-400",
       border: "border-pink-500",
+      hover: "hover:bg-pink-500",
     },
     {
       mascot: class5,
       color: "bg-green-400",
       border: "border-green-500",
+      hover: "hover:bg-green-500",
     },
   ];
   return (
@@ -47,13 +53,16 @@ export default function Dashboard() {
         </h1>
         <div className="flex flex-row items-center justify-center gap-6 flex-wrap">
           {classes.map((cls, i) => (
-            <button
+            <Link
+              href={"/class/" + (i + 1)}
               key={i}
               className={
-                "flex flex-col items-center gap-2 p-8 rounded border-4 h-48 aspect-square " +
+                "flex flex-col items-center gap-2 p-8 rounded border-4 sm:w-56 h-fit w-40 aspect-square transition-all " +
                 cls.color +
                 " " +
-                cls.border
+                cls.border +
+                " " +
+                cls.hover
               }
             >
               <Image
@@ -62,7 +71,7 @@ export default function Dashboard() {
                 className="w-full"
               />
               <h1 className="text-white font-semibold">{"Class-" + (i + 1)}</h1>
-            </button>
+            </Link>
           ))}
         </div>
       </div>
