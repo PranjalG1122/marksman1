0f226332c01f1638291169dbc8e3810654d002b7
LakshyaGupta26 <lakshyagupta2602@gmail.com>
2024-07-03 14:36:29 +0530

    refactor: Update Quiz page layout and styling

--- app/quiz/[quizId]/page.tsx
+++ app/quiz/[quizId]/page.tsx
@@ -2,31 +2,30 @@ import Navbar from "@/components/Navbar";
 
 export default function Quiz({ params }: { params: { quizId: string } }) {
   return (
-    <div className="flex min-h-screen flex-col items-center w-full">
-      <Navbar />
-      <div className="w-full px-4 lg:px-lg py-lg flex flex-col lg:flex-row justify-start items-start gap pt-16">
-        <div className="w-full lg:w-1/3 flex flex-col justify-start items-start gap-8">
-          <div className="text-black text-3xl lg:text-4xl font-normal font-['Inter'] mb-4">
+      <div className="w-full px-4 lg:px-lg py-lg flex flex-col lg:flex-row justify-start items-start gap pt-16 font-['Inter']">
+        <Navbar />
+        <div className="h-full w-full lg:w-auto flex flex-col justify-start items-start pr-8">
+          <div className="text-black text-2xl lg:text-3xl font-normal  mb-4">
             Chapter 1: Numbers
           </div>
-          <div className="flex flex-col justify-start items-start gap-4">
-            <div className="w-full p-4 bg-slate-400 rounded-lg flex justify-center items-center">
-              <div className="text-black text-lg lg:text-xl font-normal font-['Inter'] text-center">
+          <div className="flex flex-col justify-end items-start gap-4 h-full">
+            <div className="p-4 bg-slate-400 rounded-lg flex justify-center items-center">
+              <div className="text-black text-lg lg:text-xl font-normal  text-center">
                 Hint: look at the number of berries
               </div>
             </div>
             <img
-              className="w-full max-w-xs lg:max-w-sm mx-auto"
+              className="w-full max-w-xs lg:max-w-sm mx-auto rounded-lg shadow-md"
               src="https://via.placeholder.com/276x355"
               alt="Hint"
             />
           </div>
         </div>
-        <div className="w-full lg:flex-1 p-lg lg:p-lg flex flex-col justify-start items-start gap-4">
-          <div className="w-full text-center text-black text-2xl lg:text-3xl  font-semibold font-['Inter']">
+        <div className="w-full lg:flex-1 p-lg lg:p-lg flex flex-col justify-center items-center gap-8 ">
+          <div className="w-full text-center text-black text-3xl lg:text-3xl  font-semibold ">
             Course Challenge
           </div>
-          <div className="w-full text-black text-lg lg:text-xl font-medium font-['Inter'] mb-4">
+          <div className="w-full text-black text-lg lg:text-xl font-medium  mb-4">
             A mother bear has 42 berries and her baby bear collected 44 berries.
             Who collected more berries?
           </div>
@@ -44,7 +43,7 @@ export default function Quiz({ params }: { params: { quizId: string } }) {
             <div className="bg-white flex items-center gap-4 p-2 rounded-lg shadow-md">
               <div className="w-6 h-6 bg-indigo-400 rounded-full border border-black flex justify-center items-center">
                 <div className="text-center text-black text-sm font-semibold">
-                  1
+                  2
                 </div>
               </div>
               <div className="text-black text-lg font-normal">
@@ -54,7 +53,7 @@ export default function Quiz({ params }: { params: { quizId: string } }) {
             <div className="bg-white flex items-center gap-4 p-2 rounded-lg shadow-md">
               <div className="w-6 h-6 bg-white rounded-full border border-black flex justify-center items-center">
                 <div className="text-center text-black text-sm font-semibold">
-                  1
+                  3
                 </div>
               </div>
               <div className="text-black text-lg font-normal">
@@ -63,8 +62,8 @@ export default function Quiz({ params }: { params: { quizId: string } }) {
             </div>
           </div>
         </div>
-        <div className="w-full lg:w-1/4 flex flex-col justify-items-end  items-start gap-8">
-          <div className="text-black text-2xl lg:text-3xl font-normal">
+        <div className="w-full lg:w-1/4 flex flex-col justify-items-end  items-start gap-8 pl-8">
+          <div className="text-black text-3xl lg:text-2xl font-normal">
             Questions Attempted
           </div>
           <div className="flex gap-2">
@@ -82,6 +81,5 @@ export default function Quiz({ params }: { params: { quizId: string } }) {
           </div>
         </div>
       </div>
-    </div>
   );
 }
